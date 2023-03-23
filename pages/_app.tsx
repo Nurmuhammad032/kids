d233194c641ff22489e3cfd58eb9457875d7c5ec
Nurmuhammad <nurmuhammad2003o@gmail.com>
2023-03-24 00:25:31 +0500

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Navbar } from "../components";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Endomarine Kids</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
